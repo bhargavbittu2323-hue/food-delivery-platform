145f28e0198756f96b2372a3e5b18fd2acb6958e
Bhargavbittu-hue <Bhargavbittu2323@gmail.com>
2026-06-08 11:45:56 +0530

    First Commit

---
+++ auth-service/src/main/java/com.foodapp.auth/dto/response/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.foodapp.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private String role;
+
+}
